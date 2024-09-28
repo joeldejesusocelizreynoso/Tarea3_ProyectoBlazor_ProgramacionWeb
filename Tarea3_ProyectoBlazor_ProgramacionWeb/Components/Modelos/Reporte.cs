1c6a8dff50c885880f966a19149e16c4e9e15080
Joel De Jesús Oseliz Reynoso <joeldejesusocelizreynoso@gmail.com>
2024-09-27 23:04:50 -0400

    Agregar archivos de proyecto.

---
+++ Tarea3_ProyectoBlazor_ProgramacionWeb/Components/Modelos/Reporte.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+class Reporte
+{
+
+
+    public string Id { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "El campo fecha es requerido")]
+    public DateTime Fecha { get; set; } = DateTime.Now;
+
+    [Required(ErrorMessage = "El campo Descripcion es requerido")]
+    [MinLength(10, ErrorMessage = "la descripcion debe tener al menos 10 caractere")]
+
+    public string Descripcion { get; set; } = "";
+    public double CostoEstimado { get; set; } = 0;
+    public int Muertos { get; set; } = 0;
+    public int Heridos { get; set; } = 0;
+    public int VehiculosInvolucrados { get; set; } = 0;
+}
